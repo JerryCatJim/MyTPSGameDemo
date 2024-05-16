6a1b60713eb3fec8d24b0cd0f4984ab67287f6dd
JerryCatJim <jinnanlin@163.com>
2024-05-16 22:45:16 +0800

    Change .gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-MyTPSGameDemo just for practice.
--- Source/TPSGame/Private/MyStaticFunctionForLuaTest.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MyStaticFunctionForLuaTest.h"
-
-int UMyStaticFunctionForLuaTest::GetIntTest()
-{
-	return 100;
-}
