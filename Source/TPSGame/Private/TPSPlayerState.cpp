5c8bf40c87b7e29c9df08a7b0052fbb85d54f7d1
JerryCatJim <jinnanlin@163.com>
2024-05-28 19:46:55 +0800

    删除TPSGameMode，新增TPSPlayerState，增加击杀通知功能

---
+++ Source/TPSGame/Private/TPSPlayerState.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayerState.h"
+#include "SCharacter.h"
+
+#include "Net/UnrealNetwork.h"
+
+void ATPSPlayerState::Reset()
+{
+	Super::Reset();
+}
+
+void ATPSPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//DOREPLIFETIME(ATPSPlayerState, Kills);
+}
+
+void ATPSPlayerState::CopyProperties(APlayerState* PlayerState)
+{
+	Super::CopyProperties(PlayerState);
+
+	ASCharacter* Player = Cast<ASCharacter>(GetInstigator());
+	if(Player && Player->GetInventoryComponent())
+	{
+		if(Player->GetInventoryComponent()->ItemList.Num() > 0)
+		{
+			ItemList = Player->GetInventoryComponent()->ItemList;
+			ItemList.Shrink();
+		}
+		else
+		{
+			ItemList.Empty();
+		}
+	}
+}
---
+++ Source/TPSGame/Public/TPSPlayerState.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "Component/InventoryComponent.h"
+#include "TPSPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSGAME_API ATPSPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Reset() override;
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
+
+protected:
+	virtual void CopyProperties(APlayerState* PlayerState) override;
+
+public:
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int PersonalScore = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	int Kills = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Deaths = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int TeamID = -1;*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	TArray<FMyItem> ItemList;
+};
