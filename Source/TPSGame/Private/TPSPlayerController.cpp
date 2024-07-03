af47e090f0bf7ffbc7d306912c83243a92d508fd
JerryCatJim <jinnanlin@163.com>
2024-07-03 18:59:25 +0800

    新增游戏内倒计时功能，并且消除服务器和客户端的时间误差

--- Source/TPSGame/Private/TPSPlayerController.cpp
+++ Source/TPSGame/Private/TPSPlayerController.cpp
@@ -8,3 +8,56 @@ ATPSPlayerController::ATPSPlayerController()
 	//背包组件初始化
 	InventoryComponent = CreateDefaultSubobject<UInventoryComponent>(TEXT("InventoryComponent"));
 }
+
+void ATPSPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	CheckTimeSync(DeltaSeconds);
+}
+
+void ATPSPlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+	if(IsLocalController())  //若在服务器调用，服务器有多个Controller，需找到自己的那个
+	{
+		ServerRequestServerTime(GetWorld()->GetDeltaSeconds());
+	}
+}
+
+float ATPSPlayerController::GetServerTime()
+{
+	if(HasAuthority())
+	{
+		return GetWorld()->GetTimeSeconds();
+	}
+	else
+	{
+		return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+	}
+}
+
+void ATPSPlayerController::CheckTimeSync(float DeltaTime)
+{
+	TimeSyncRunningTime += DeltaTime;
+	if(IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency)
+	{
+		ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+		TimeSyncRunningTime = 0.f;
+	}
+}
+
+void ATPSPlayerController::ServerRequestServerTime_Implementation(float TimeOfClientRequest)
+{
+	const float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+	ClientReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
+}
+
+void ATPSPlayerController::ClientReportServerTime_Implementation(float TimeOfClientRequest,
+	float TimeServerReceivedClientRequest)
+{
+	//服务器传回的时间+服务器返回消息的延迟等于服务器当前时间，这里粗略地估算返回延迟为客户端rpc+服务器rpc延迟总和的一半
+	const float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfClientRequest;
+	const float CurrentServerTime = TimeServerReceivedClientRequest + RoundTripTime/2;
+	ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
+}
--- Source/TPSGame/Public/TPSPlayerController.h
+++ Source/TPSGame/Public/TPSPlayerController.h
@@ -19,9 +19,35 @@ class TPSGAME_API ATPSPlayerController : public APlayerController
 	
 public:
 	ATPSPlayerController();
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	/** Called after this PlayerController's viewport/net connection is associated with this player controller. */
+	virtual void ReceivedPlayer() override;  //与服务器连接完成后就调用，在这里调用同步时间是最早的
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetServerTime();
 	
+protected:
+	//同步服务器和客户端的时间(不是直接同步WorldTime，而是记录两者的差值，让客户端自己加)
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+
+	//将服务器收到请求时的WorldTime返还给对应客户端
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
+	void CheckTimeSync(float DeltaTime);
 public:
 	//背包组件
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= Component)
 	UInventoryComponent* InventoryComponent;
+
+protected:
+	float ClientServerDelta = 0.f; //客户端当前时间和服务器时间的差值
+
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 5.f;
+
+	float TimeSyncRunningTime = 0.f;  //距离上次与服务器同步时间过去了多久
 };
