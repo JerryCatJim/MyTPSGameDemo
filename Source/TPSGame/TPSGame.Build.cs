4277775ccf029bb45441ba82dae3ac54068956fc
JerryCatJim <jinnanlin@163.com>
2024-08-01 16:48:27 +0800

    在C++中启用Niagara

--- Source/TPSGame/TPSGame.Build.cs
+++ Source/TPSGame/TPSGame.Build.cs
@@ -8,7 +8,7 @@ public class TPSGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysicsCore", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysicsCore", "UMG", "Niagara" });
 		
 		PublicDependencyModuleNames.AddRange(new string[] { "OnlineSubsystemSteam", "OnlineSubsystem" });
 		
