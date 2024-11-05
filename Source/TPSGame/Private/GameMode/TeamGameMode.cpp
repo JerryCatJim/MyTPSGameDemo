f4d659078d1700ecddcf5fe52340b69767753ad6
JerryCatJim <jinnanlin@163.com>
2024-11-05 20:52:15 +0800

    新增区分不同队伍的出生点 功能

--- Source/TPSGame/Private/GameMode/TeamGameMode.cpp
+++ Source/TPSGame/Private/GameMode/TeamGameMode.cpp
@@ -5,6 +5,7 @@
 #include "TPSGameState.h"
 #include "TPSPlayerState.h"
 #include "Kismet/GameplayStatics.h"
+#include "TPSGame/PlayerStart/TeamPlayerStart.h"
 
 ATeamGameMode::ATeamGameMode()
 {
@@ -44,6 +45,65 @@ void ATeamGameMode::Logout(AController* Exiting)
 	}
 }
 
+void ATeamGameMode::RestartPlayer(AController* NewPlayer)
+{
+	//Super::RestartPlayer(NewPlayer);
+	
+	if (NewPlayer == nullptr || NewPlayer->IsPendingKillPending())
+	{
+		return;
+	}
+
+	if(!IsValid(NewPlayer->PlayerState))
+	{
+		SetTeamFailedControllers.AddUnique(NewPlayer);
+		UE_LOG(LogGameMode, Warning, TEXT("RestartPlayer: PlayerState获取失败"));
+		return;
+	}
+
+	//从所有ATeamPlayerStart子类的PlayerStart中找到 所有Team和玩家Team一样的出生点并组成数组，从中随机一个作为此次出生点
+	ATPSPlayerState* MyPlayerState = NewPlayer->GetPlayerState<ATPSPlayerState>();
+	TArray<AActor*> TeamPlayerStarts;
+	TArray<ATeamPlayerStart*> MyTeamPlayerStarts;
+	if(MyPlayerState && MyPlayerState->GetTeam() != ETeam::ET_NoTeam)
+	{
+		UGameplayStatics::GetAllActorsOfClass(this, ATeamPlayerStart::StaticClass(), TeamPlayerStarts);
+		for(AActor* Start : TeamPlayerStarts)
+		{
+			ATeamPlayerStart*  MyPlayerStart = Cast<ATeamPlayerStart>(Start);
+			if(MyPlayerStart && MyPlayerStart->Team == MyPlayerState->GetTeam())
+			{
+				MyTeamPlayerStarts.Emplace(MyPlayerStart);
+			}
+		}
+	}
+	else
+	{
+		SetTeamFailedControllers.AddUnique(NewPlayer);
+		//UE_LOG(LogGameMode, Warning, TEXT("RestartPlayer: PlayerState的Team为NoTeam，找不到对应颜色队伍的出生点"));
+		return;
+	}
+	
+	AActor* StartSpot = nullptr;
+	if(MyTeamPlayerStarts.Num() > 0 )
+	{
+		StartSpot = MyTeamPlayerStarts[FMath::RandRange(0, MyTeamPlayerStarts.Num() - 1)];
+	}
+
+	// If a start spot wasn't found,
+	if (StartSpot == nullptr)
+	{
+		// Check for a previously assigned spot
+		if (NewPlayer->StartSpot != nullptr)
+		{
+			StartSpot = NewPlayer->StartSpot.Get();
+			UE_LOG(LogGameMode, Warning, TEXT("RestartPlayer: Player start not found, using last start spot"));
+		}	
+	}
+
+	RestartPlayerAtPlayerStart(NewPlayer, StartSpot);
+}
+
 float ATeamGameMode::CalculateDamage(AController* Attacker, AController* Victim, float BaseDamage)
 {
 	if(Attacker == nullptr || Victim == nullptr) return 0;
@@ -69,13 +129,13 @@ void ATeamGameMode::HandleMatchHasStarted()
 	{
 		MyGameState = GetGameState<ATPSGameState>();
 	}
-	if(MyGameState)
+	/*if(MyGameState)
 	{
 		for(APlayerState* PS : MyGameState->PlayerArray)
 		{
 			TryAddAndSetTeam(PS);
 		}
-	}
+	}*/
 }
 
 void ATeamGameMode::TryAddAndSetTeam(APlayerState* NewPlayerState)
@@ -95,5 +155,15 @@ void ATeamGameMode::TryAddAndSetTeam(APlayerState* NewPlayerState)
 			MyGameState->BlueTeam.AddUnique(MyPS);
 			MyPS->SetTeam(ETeam::ET_BlueTeam);
 		}
+	}
+
+	//客户端刚进入游戏时先触发了RestartPlayer然后才触发Login(?)，所以把玩家分配到对应队伍出生点不会成功，
+	//记录设置失败的玩家，等Login后调用TryAddAndSetTeam时再重新RestartPlayer一次
+	//客户端触发顺序为 : RestartPlayer -> HandleMatchHasStarted -> PostLogin (???)
+	for(int i = SetTeamFailedControllers.Num() - 1; i >=0; --i)
+	{
+		AController* TempController = SetTeamFailedControllers[i];
+		SetTeamFailedControllers.RemoveAt(i);
+		RestartPlayer(TempController);
 	}
 }
--- Source/TPSGame/Public/GameMode/TeamGameMode.h
+++ Source/TPSGame/Public/GameMode/TeamGameMode.h
@@ -20,6 +20,8 @@ public:
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 	virtual void Logout(AController* Exiting) override;
 
+	virtual void RestartPlayer(AController* NewPlayer) override;
+	
 	virtual float CalculateDamage(AController* Attacker, AController* Victim, float BaseDamage) override;
 	
 protected:
@@ -27,4 +29,11 @@ protected:
 
 private:
 	void TryAddAndSetTeam(APlayerState* NewPlayerState);
+
+private:
+	//客户端刚进入游戏时先触发了RestartPlayer然后才触发Login(?)，所以把玩家分配到对应队伍出生点不会成功，
+	//记录设置失败的玩家，等Login后调用TryAddAndSetTeam时再重新RestartPlayer一次
+	//客户端触发顺序为 : RestartPlayer -> HandleMatchHasStarted -> PostLogin (???)
+	UPROPERTY()
+	TArray<AController*> SetTeamFailedControllers;
 };
