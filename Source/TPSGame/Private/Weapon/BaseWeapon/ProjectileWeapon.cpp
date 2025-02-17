b7706b5b163f5e5ab356d1e0da88b1031f7d5a90
JerryCatJim <jinnanlin@163.com>
2025-02-17 18:57:03 +0800

    暂时实现抛物线绘制功能(目前为绘制Debug线条)，消除了抛物线的抖动

--- Source/TPSGame/Private/Weapon/BaseWeapon/ProjectileWeapon.cpp
+++ Source/TPSGame/Private/Weapon/BaseWeapon/ProjectileWeapon.cpp
@@ -4,10 +4,26 @@
 #include "Weapon/Projectile/Projectile.h"
 #include "Engine/SkeletalMeshSocket.h"
 #include "DrawDebugHelpers.h"  //射线检测显示颜色
+#include "SCharacter.h"
+#include "Kismet/GameplayStatics.h"
 
 AProjectileWeapon::AProjectileWeapon()
 {
 	WeaponBulletType = EWeaponBulletType::Projectile;
+}
+
+void AProjectileWeapon::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	DrawMovementTrajectory();
+}
+
+void AProjectileWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	MuzzleSocket = GetWeaponMeshComp()->GetSocketByName(MuzzleSocketName);
 }
 
 void AProjectileWeapon::DealFire()
@@ -16,7 +32,7 @@ void AProjectileWeapon::DealFire()
 	//与射线检测的即时命中武器不同，发射器武器完全重写发射逻辑
 
 	APawn* InstigatorPawn = Cast<APawn>(GetOwner());
-	const USkeletalMeshSocket* MuzzleSocket = GetWeaponMeshComp()->GetSocketByName(MuzzleSocketName);
+	//const USkeletalMeshSocket* MuzzleSocket = GetWeaponMeshComp()->GetSocketByName(MuzzleSocketName);
 
 	//没有MuzzleSocket的武器无法发射子弹
 	if(!MuzzleSocket)
@@ -29,7 +45,7 @@ void AProjectileWeapon::DealFire()
 	FTransform SocketTransform = MuzzleSocket->GetSocketTransform(GetWeaponMeshComp());
 	//枪口 指向 准星瞄准方向的终点 的向量
 	FVector ToTarget = GetCurrentAimingPoint() - SocketTransform.GetLocation();
-	FRotator TargetRotation = ToTarget.Rotation();
+	FRotator TargetRotation = FRotator(ToTarget.Rotation().Pitch + UpAngelOffset, ToTarget.Rotation().Yaw, ToTarget.Rotation().Roll);
 	if(ProjectileClass && InstigatorPawn)
 	{
 		UWorld* World = GetWorld();
@@ -57,4 +73,57 @@ void AProjectileWeapon::DealFire()
 	}
 
 	//DrawDebugLine(GetWorld(), SocketTransform.GetLocation(), GetCurrentAimingPoint(), FColor::Red, false, 1.0f, 0,1.0f);
+}
+
+void AProjectileWeapon::DrawMovementTrajectory()
+{
+	if(!CanShowMovementTrajectory || !IsValid(MyOwner) || MyOwner->GetIsDied()) return;
+
+	if(DrawTrajectoryLocally && !MyOwner->IsLocallyControlled()) return;
+	
+	//没有MuzzleSocket的武器无法发射子弹
+	if(!MuzzleSocket)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red,
+					FString::Printf(TEXT("当前武器未设置MuzzleFlash位置!")));
+		return;
+	}
+	if(!OverrideProjectileDefaultData)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red,
+					FString::Printf(TEXT("武器中的速度和重力系数未覆盖给子弹，无法描绘预测轨迹!")));
+		return;
+	}
+	
+	if(MyOwner->GetIsAiming() && !MyOwner->GetIsReloading())
+	{
+		FHitResult HitResult;
+		TArray<FVector> OutPoints;
+		FVector LastTraceDestination;
+		FVector StartPos = MuzzleSocket->GetSocketLocation(GetWeaponMeshComp());
+		FVector LaunchVelocity(((GetCurrentAimingPoint(false) - StartPos).Rotation()+FRotator(UpAngelOffset,0,0)).Vector().GetSafeNormal() * ProjectileInitialSpeed);
+		TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+		ObjectTypes.Emplace(ECC_WorldStatic);
+		ObjectTypes.Emplace(ECC_WorldDynamic);
+		ObjectTypes.Emplace(ECC_Pawn);
+		TArray<AActor*> ActorsToIgnore;
+		UGameplayStatics::Blueprint_PredictProjectilePath_ByObjectType(
+			this,
+			HitResult,
+			OutPoints,
+			LastTraceDestination,
+			StartPos,
+			LaunchVelocity,
+			true,
+			3,
+			ObjectTypes,
+			false,
+			ActorsToIgnore,
+			EDrawDebugTrace::ForOneFrame,
+			0,
+			DrawFrequency,
+			DrawTrajectoryTime,
+			GetWorld()->GetWorldSettings()->WorldGravityZ * ProjectileGravityZScale
+			);
+	}
 }
--- Source/TPSGame/Public/Weapon/BaseWeapon/ProjectileWeapon.h
+++ Source/TPSGame/Public/Weapon/BaseWeapon/ProjectileWeapon.h
@@ -18,10 +18,17 @@ public:
 	AProjectileWeapon();
 	
 protected:
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void BeginPlay() override;
+	
 	virtual void DealFire() override;
 
+private:
+	void DrawMovementTrajectory();
+	
 public:
-
+	
 protected:
 	UPROPERTY(EditAnywhere, Category= "Weapon")
 	TSubclassOf<class AProjectile> ProjectileClass;
@@ -34,4 +41,19 @@ protected:
 	float ProjectileMaxSpeed = 50.f;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=WeaponProjectile)  //是否使用武器设置的数值覆盖其发射的子弹类的ProjectileMovementComponent的默认数值
 	bool OverrideProjectileDefaultData = false;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=WeaponProjectile)  //武器是否显示抛物线轨迹
+	bool CanShowMovementTrajectory = false;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=WeaponProjectile)  //子弹生成时相对枪口上扬的角度
+	float UpAngelOffset = 0.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=WeaponProjectile, meta=(ClampMin = 1.f, ClampMax = 999.f))  //生成抛物线离散点的频率
+	float DrawFrequency = 99.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=WeaponProjectile, meta=(ClampMin = 0.1f, ClampMax = 5.f))  //总共绘制模拟子弹飞行设定秒数的抛物线
+	float DrawTrajectoryTime = 2.f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=WeaponProjectile)  //是否只在本地绘制而不同步到服务器
+	bool DrawTrajectoryLocally = true;
+	
+private:
+	UPROPERTY()
+	const USkeletalMeshSocket* MuzzleSocket;
 };
